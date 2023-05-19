8b197d8e7a7013f7f73d44e716e065485ab458ab
NothingTiananmen1989 <kumipon@yandex.ru>
2023-05-19 18:05:35 +0300

    Update 9 (2007).py

--- tasks_python_1/9 (2007).py
+++ tasks_python_1/9 (2007).py
@@ -1,18 +1,20 @@
 x = str(input())
 f = ''
-f += x[0].lower()
-for i in x[1:]:
-    if f[-1].islower():
-        f += i.upper()
-        continue
-    if f[-1].isupper():
-        f += i.lower()
-        continue
-    if f[-1] == ' ':
-        if f[-2].islower():
+def r(f):
+    f += x[0].lower()
+    for i in x[1:]:
+        if f[-1].islower():
             f += i.upper()
             continue
-        if f[-2].isupper():
+        if f[-1].isupper():
             f += i.lower()
             continue
-print(f)
+        if f[-1] == ' ':
+            if f[-2].islower():
+                f += i.upper()
+                continue
+            if f[-2].isupper():
+                f += i.lower()
+                continue
+    return f
+print(r(f))
